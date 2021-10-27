1157ffe903e0637d712a5f6ae8560a620b787b47
Thiagodsla <thiagod1192@gmail.com>
2021-10-27 19:59:40 -0300

    inicio do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1> Calculadora </h1>
+  <Calculadora  />
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculadora from './components/Calculadora.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculadora
   }
-}
+};
 </script>
 
 <style>
@@ -21,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="content">
+  <div class="corpo">
+    
+    <div class="visor">
+      conteudo do visor
+    </div>
+    
+  <div class="botoes">
+    botoes
+  </div>
+
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Calculadora',
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+
+h1 {
+  color:aliceblue
+}
+
+.content{
+  display: flex;
+  justify-content: center;
+}
+.corpo{
+  background: red;
+  width: 40rem;
+  height: 45rem;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
